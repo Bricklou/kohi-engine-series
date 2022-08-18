e91eda7e07aaade986b4fccda10b0b3419112bc1
Louis Bailleau <louis.bailleau@free.fr>
2022-08-18 03:36:31 +0200

    kohi: video 4

--- testbed/src/main.c
+++ testbed/src/main.c
@@ -1,6 +1,9 @@
 #include <core/asserts.h>
 #include <core/logger.h>
 
+// TODO: test
+#include <platform/platform.h>
+
 int main(void) {
   KFATAL("A test message: %f", 3.14f);
   KERROR("A test message: %f", 3.14f);
@@ -10,5 +13,15 @@ int main(void) {
   KTRACE("A test message: %f", 3.14f);
 
   KASSERT(1 == 0);
+
+  platform_state state;
+  if (platform_startup(&state, "Kohi Engine Testbed", 100, 100, 1280, 720)) {
+    while (TRUE) {
+      platform_pump_message(&state);
+    }
+  }
+
+  platform_shutdown(&state);
+
   return 0;
 }
